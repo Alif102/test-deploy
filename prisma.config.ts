e55fd2ca7424895aae65a5e96223d73a44f238e5
Alif Ahmed <alifahmed102@gmail.com>
2026-02-11 12:40:49 +0600

    renderrrr

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,5 @@
-import 'dotenv/config'
-import { defineConfig, env } from 'prisma/config'
+import 'dotenv/config';
 
-export default defineConfig({
+export default {
   schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-  },
-  datasource: {
-    url: env('DATABASE_URL'),
-  },
-})
+};
